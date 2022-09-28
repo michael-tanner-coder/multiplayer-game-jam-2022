923a38ba4268776a1184b13616cd9b9b12db477a
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2022-09-27 21:27:56 -0500

    Created data for mobility powerups

--- MultiplayerGameJam/Assets/Scripts/PartScriptableObject.cs
+++ MultiplayerGameJam/Assets/Scripts/PartScriptableObject.cs
@@ -6,6 +6,34 @@ public enum PartType {MOBILITY, WEAPON, TARGETING};
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/PartScriptableObject", order = 1)]
 public class PartScriptableObject : ScriptableObject
 {
+    [Header("Part Type")]
     public string name;
     public PartType type;
+
+    [Header("Movement")]
+    public float acceleration;
+    public float braking;
+    public float maxSpeed;
+    public float boostPower;
+    public float boostRechargeTime;
+    public float maxFlySpeed;
+    public bool hasBoost;
+    public bool canFly;
+
+    [Header("Firing")]
+    public float fireRate;
+    public float reloadTime;
+    public float recoil;
+    public bool automatic;
+    public float chargeTime;
+    public GameObject projectile;
+
+    [Header("Targeting")]
+    public int targetCount;
+    public float targetingDelay;
+    public float targetRange;
+    public float rechargeTime;
+    public float damageDebuff;
+    public bool isAutoTarget;
+
 }
